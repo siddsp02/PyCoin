de9526a329d3b4f62a289badc68c7cfc348c787a
siddsp <35938702+siddsp02@users.noreply.github.com>
2023-01-11 12:52:23 -0700

    Moved header functions to miner.

--- src/miner.py
+++ src/miner.py
@@ -10,16 +10,53 @@ timestamp updates -- the whole thing.
 """
 
 import json
+import math
 import multiprocessing as mp
 import struct
 import time
 from datetime import datetime
 from functools import partial, singledispatch
 
-from header import target, verify
+from .utils import sha256d
 
 UINT32_MAX = 0xFFFFFFFF
 WORKERS = mp.cpu_count()
+
+
+def verify(header: bytes) -> bool:
+    """Verify a block header using the block hashing algorithm for Bitcoin."""
+    hashed_header = sha256d(header)
+    bits = struct.unpack_from("<I", header, offset=72)[0]
+    return int.from_bytes(hashed_header, byteorder="little") < target(bits)
+
+
+def difficulty(bits: int) -> float:
+    """Returns the difficulty given the target in compact format.
+
+    References:
+        - https://en.bitcoin.it/wiki/Difficulty
+    """
+    exponent_diff = 8 * (0x1D - ((bits >> 24) & 0xFF))
+    significand = bits & 0xFFFFFF
+    return math.ldexp(0xFFFF / significand, exponent_diff)
+
+
+def target(bits: int) -> int:
+    """Returns the target given its compact format value.
+
+    References:
+        - https://en.bitcoin.it/wiki/Difficulty
+    """
+    p = bits & 0xFFFFFF
+    q = (bits & 0xFF000000) >> 24
+    return p * 2 ** (8 * (q - 3))
+
+
+def hashrate_from_difficulty(diff: float) -> float:
+    """Returns an estimate of the network hashrate when given
+    the difficulty or target in non-compact form.
+    """
+    return (diff / 600) * 2**32
 
 
 def update_timestamp(block_header: bytearray) -> None:
