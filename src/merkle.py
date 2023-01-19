4adc5af8c93de6dab78531f8c33f544a44f7433c
siddsp <35938702+siddsp02@users.noreply.github.com>
2023-01-19 11:35:47 -0700

    Updated functions and doctest.

--- src/merkle.py
+++ src/merkle.py
@@ -21,14 +21,12 @@ MerkleProof = tuple[ProofList, PairList]
 T = TypeVar("T", bound=bytes)
 
 
-def pairs(values: Sequence[bytes]) -> Iterator[bytes]:
+def pairs(values: Sequence[bytes]) -> Iterator[tuple[bytes, bytes]]:
     """Groups a sequence of bytes into pairs. If the length of
     the sequence is odd, the last item is paired with itself.
     """
-    evens, odds = values[0::2], values[1::2]
-    longest = max(evens, odds, key=len)
-    last = longest[-1]
-    return zip_longest(evens, odds, fillvalue=last)  # type: ignore
+    args, last = [iter(values)] * 2, values[-1]
+    return zip_longest(*args, fillvalue=last)  # type: ignore
 
 
 # Hashing multiple pairs maybe computationally expensive,
@@ -53,12 +51,13 @@ def hash_tree(merkle_tree: Iterable[bytes]) -> bytes:
 
     Examples:
     >>> # Block 125552 on Bitcoin:
-    >>> txs = ["51d37bdd871c9e1f4d5541be67a6ab625e32028744d7d4609d0c37747b40cd2d",\
+    >>> txs = map(bytes.fromhex,                                                  \
+              ["51d37bdd871c9e1f4d5541be67a6ab625e32028744d7d4609d0c37747b40cd2d",\
                "60c25dda8d41f8d3d7d5c6249e2ea1b05a25bf7ae2ad6d904b512b31f997e1a1",\
                "01f314cdd8566d3e5dbdd97de2d9fbfbfd6873e916a00d48758282cbb81a45b9",\
-               "b519286a1040da6ad83c783eb2872659eaf57b1bec088e614776ffe7dc8f6d01"]
-    >>> tx_bytes = map(bytes.fromhex, txs)
-    >>> result = hash_tree(tx_bytes)
+               "b519286a1040da6ad83c783eb2872659eaf57b1bec088e614776ffe7dc8f6d01"]\
+        )
+    >>> result = hash_tree(txs)
     >>> result.hex()  # Convert results.
     '2b12fcf1b09288fcaff797d71e950e71ae42b91e8bdb2304758dfcffc2b620e3'
 
@@ -68,8 +67,7 @@ def hash_tree(merkle_tree: Iterable[bytes]) -> bytes:
     """
     tree = list(merkle_tree)
     while len(tree) > 1:
-        tree[:] = pairs(tree)
-        tree[:] = starmap(hash_pair, tree)
+        tree[:] = starmap(hash_pair, pairs(tree))
     return tree.pop()
 
 
@@ -97,19 +95,18 @@ def create_proof(merkle_tree: Sequence[bytes], tx: bytes) -> MerkleProof:
     combined_pairs = []
     proof_sequence = []
     while len(tree) > 1:
-        tree[:] = pairs(tree)
         # Find the next pair which includes our search element,
         # and append the sibling node to the proof list.
-        pair = next(pair for pair in tree if item in pair)
+        pair = next(pair for pair in pairs(tree) if item in pair)
         (left, right) = pair
         # Direction is also added to the proof list, so the order
         # of concatenation is known to anyone trying to verify the
         # proof itself.
         proof_element = ("right", right) if item == left else ("left", left)
-        item = hash_pair(*pair)
+        item = hash_pair(left, right)
         proof_sequence.append(proof_element)
         combined_pairs.append(item)
-        tree[:] = starmap(hash_pair, tree)
+        tree[:] = starmap(hash_pair, pairs(tree))
     return proof_sequence, combined_pairs
 
 
