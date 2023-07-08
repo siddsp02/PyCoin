ccb01f898f3abf2d07a98b2a8e210a37b6db445f
Sidd <sidd.s.pai@gmail.com>
2023-07-07 23:24:16 -0600

    Removed script tests (will be re-added).

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,16 +1,1 @@
-from src.script import run, stack
 
-
-def test_run() -> None:
-    # Basic arithmetic operations.
-    # Addition.
-    assert run("2 7 OP_ADD") == 9
-    assert run("4 9 OP_ADD") == 13
-    assert run("1 2 OP_ADD 2 OP_ADD") == 5
-    # Subtraction.
-    assert run("10 5 OP_SUB") == 5
-    # Equality.
-    # Check that a boolean is returned and not just a non-zero value.
-    assert run("4 5 OP_EQUAL") == False
-    assert run("5 5 OP_EQUAL") == True
-    assert run("5 5 OP_ADD 10 OP_EQUAL") == True
