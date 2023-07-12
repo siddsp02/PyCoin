e8ff68d5c380b92c9366cf89d377906ec9571c5f
Sidd <sidd.s.pai@gmail.com>
2023-07-12 01:46:14 -0600

    Updated script tests.

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,1 +1,12 @@
+from src.script.script import stack, OPCODE_MAP
 
+
+def test_basic_ops_with_stack() -> None:
+    stack.append(3)
+    stack.append(4)
+    OPCODE_MAP["OP_ADD"]()
+    assert stack.pop() == 7
+    stack.append(5)
+    stack.append(10)
+    OPCODE_MAP["OP_SUB"]()
+    assert stack.pop() == 5
