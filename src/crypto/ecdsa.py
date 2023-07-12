10fff56bea793ae22eb82cda5bb6c31fb6fee828
Sidd <sidd.s.pai@gmail.com>
2023-07-12 01:45:28 -0600

    Updated ecdsa to use generators instead of lists.

--- src/crypto/ecdsa.py
+++ src/crypto/ecdsa.py
@@ -1,97 +1,88 @@
 # !usr/bin/env python3
 
+import base64
 import multiprocessing as mp
 import random
-import time
+import struct
+from itertools import repeat
+from typing import Iterable, NamedTuple, Self
 
+from src.constants import WORKERS
+from src.encoding_schemes import der
+from src.utils import extract_bits, int_to_bytes_big, modinv, sha256d
 
-from .secp256k1 import CURVE, Point
-from ..utils import extract_bits, sha256d
+from .secp256k1 import INFINITY, G, N, P, Point
 
-WORKERS = mp.cpu_count()
+random.seed(1)
 
-p, a, b, g, n, h = CURVE
+# fmt: off
 
-G = Point.from_int(g)
+class Signature(NamedTuple):
+    r: int
+    s: int
+    
+    def base64(self, pubkey: Point) -> bytes:
+        x, y = pubkey.affine()
+        r, s = self
+        if r < P-N:
+            header_byte = 0x30 if y % 2 == 0 else 0x27
+        else:
+            header_byte = 0x28 if y % 2 == 0 else 0x29
+        sigbin = struct.pack(">B32s32s", header_byte, int_to_bytes_big(r), int_to_bytes_big(s))
+        return base64.encodebytes(sigbin)
+    
+    @classmethod
+    def generate(cls, key: int, msg: bytes = b"") -> Self:
+        z = extract_bits(sha256d(msg), start=0, end=256)
+        r = s = 0  # Start with invalid values by default.
+        while r == 0 or s == 0:
+            k = random.randint(1, N-1)
+            x, _ = (k*G).affine()
+            r = x % N
+            s = modinv(k, N) * (z + r*key) % N
+        return cls(r, s)
+    
+    def verify(self, pubkey: Point, msg: bytes) -> bool:
+        if not pubkey.on_curve or pubkey == INFINITY:
+            return False
+        z = extract_bits(sha256d(msg), start=0, end=256)
+        r, s = self
+        s1 = modinv(s, N)
+        u1, u2 = z*s1 % N, r*s1 % N
+        point = u1*G + u2*pubkey
+        x, y = point.affine()
+        return r == x % N
+    
+    @classmethod
+    def decode(cls, sig: bytes) -> Self:
+        """Decodes a DER encoded signature. """
+        return cls._make(der.decode(sig))
 
+    @classmethod
+    def generate_many(
+        cls, amount: int, msgs: Iterable[bytes] | None = None
+    ) -> Iterable[tuple[Self, Point, bytes]]:
+        if msgs is None:
+            msgs = repeat(random.randbytes(10), amount)
+        keys = (random.randint(1, N-1) for _ in range(amount))
+        pubkeys = (k*G for k in keys)
+        chunksize = amount // WORKERS
+        with mp.Pool() as pool:
+            sigs = pool.starmap(cls.generate, zip(keys, msgs), chunksize)
+        return zip(sigs, pubkeys, msgs)
 
-def generate(privkey: int, message: bytes = b"") -> tuple[int, int]:
-    """Signs a message when given a private key, returning the signature of
-    the signed message in the form of a an integer pair (r, s).
+    @classmethod
+    def verify_all(
+        cls, params: Iterable[tuple[Self, Point, bytes]], chunksize: int = 1
+    ) -> bool:
+        if chunksize <= 0:
+            raise ValueError(
+                f"chunksize must be positive. Got a value of {chunksize} instead."
+            )
+        with mp.Pool() as pool:
+            results = pool.starmap(cls.verify, params, chunksize)
+        return all(results)
 
-    References:
-        - https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
-    """
-    z = extract_bits(sha256d(message), start=0, end=256)
-    (r, s) = (0, 0)  # Start with invalid values by default.
-    while r == 0 or s == 0:
-        k = random.randrange(1, n)
-        (x, y) = (k * G).affine()  # type: ignore
-        r = x % n
-        s = pow(k, -1, n) * (z + r * privkey) % n
-    return (r, s)
-
-
-def verify(signature: tuple[int, int], pubkey: Point, message: bytes) -> bool:
-    """Verifies that the message given was signed by the given public key.
-
-    References:
-        - https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm
-    """
-    if not pubkey.on_curve or pubkey == (0, 1, 0):
-        return False
-    z = extract_bits(sha256d(message), start=0, end=256)
-    (r, s) = signature
-    s1 = pow(s, -1, n)
-    u1, u2 = (z * s1) % n, (r * s1) % n
-    point = u1 * G + u2 * pubkey
-    if point == (0, 1, 0):
-        return False
-    (x, y) = point.affine()  # type: ignore
-    return r == x % n
-
-
-def generate_sigs(
-    amount: int,
-) -> list[tuple[tuple[int, int], tuple[int, ...] | int, bytes]]:
-    cores = mp.cpu_count()
-    msgs = [random.randbytes(10)] * amount
-    keys = [random.randint(1, n - 1) for _ in range(amount)]
-    pubkeys = [k * G for k in keys]
-    with mp.Pool() as pool:
-        sigs = pool.starmap(generate, zip(keys, msgs), chunksize=amount // cores)
-    return list(zip(sigs, pubkeys, msgs))
-
-
-def verify_sigs(
-    params: list[tuple[tuple[int, int], tuple[int, ...] | int, bytes]]
-) -> list[bool]:
-    cores = mp.cpu_count()
-    amount = len(params)
-    with mp.Pool() as pool:
-        results = pool.starmap(verify, params, chunksize=amount // cores)
-    return results
-
-
-# Signature verification is likely to be slow due to
-# the inherent lack of speed for multiprecision math.
-# On my 8 core machine, I was able to increase signature
-# verification rate from ~50/s to ~2,000/s using Projective
-# coordinates instead of affine coordinates, as well as
-# parallel processing.
-def main() -> None:
-    # A small signature verification benchmark.
-    amount = 10_000
-    s = time.perf_counter()
-    sigs = generate_sigs(amount)
-    e = time.perf_counter()
-    print(e - s)
-    s = time.perf_counter()
-    verified = verify_sigs(sigs)
-    e = time.perf_counter()
-    print(amount / (e - s))
-    print(all(verified))
-
-
-if __name__ == "__main__":
-    main()
+    def encode(self) -> bytes:
+        """Encodes a signature using DER encoding."""
+        return der.encode(self)
